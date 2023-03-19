a41c66937e409a8722d0160bcb4075fbede94aae
Da'Ronn <daronnceus@gmail.com>
2023-03-19 19:42:56 -0400

    feat: add poll command

---
+++ commands/poll.js
@@ -0,0 +1,20 @@
+const { PermissionFlagsBits, ContextMenuCommandBuilder, ApplicationCommandType } = require('discord.js');
+const guildSchema = require('../schemas/guild-schema')
+const suggestionSchema = require('../schemas/suggestion-schema')
+
+module.exports = {
+	data: new ContextMenuCommandBuilder()
+		.setName('Poll')
+        .setType(ApplicationCommandType.Message)
+        .setDefaultMemberPermissions(PermissionFlagsBits.AddReactions)
+        .setDMPermission(false),
+	async execute(interaction) {
+        const { targetMessage: message, channel, guild } = interaction
+
+        if (!channel.permissionsFor(guild.members.me).has(PermissionFlagsBits.AddReactions)) await interaction.reply({ content: `Please grant me the Add Reactions permission for this channel.`, ephemeral: true })
+
+        await message.react('<:yes:1084332973870026892>')
+        await message.react('<:no:1084332972683051068>')
+        await interaction.reply({ content: 'The poll has began.', ephemeral: true })
+    },
+}
