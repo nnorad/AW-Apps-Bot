ee56ba12379cb66aef16a510b3a5fff468f9ba08
Da'Ronn <daronnceus@gmail.com>
2023-03-23 23:09:26 -0400

    fix: update schemas

--- schemas/guild-schema.js
+++ schemas/guild-schema.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose')
 
 const guildSchema = mongoose.Schema({
     guildId: String,
-    suggestionLogId: String,
+    suggestionChannelId: String,
     suggestionId: Number,
     reactionLogId: String,
     reactionLogWebhookId: String,
-    reactionLogWebhookToken: String
+    reactionLogWebhookToken: String,
+    giveawayChannelId: String
 })
 
 module.exports = mongoose.model('guilds', guildSchema)
